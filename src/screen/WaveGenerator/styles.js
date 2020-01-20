5d4d5e8f1dcb5bd07a74bbb8a2dc19aaa9ada031
Nitin Madhukar <iec2018053@iiita.ac.in>
2020-01-20 14:29:54 +0530

    board layouts changes

--- src/screen/WaveGenerator/styles.js
+++ src/screen/WaveGenerator/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   height: 100%;
-  display: flex;
+  display: relative;
   justify-content: center;
   align-items: center;
   background: ${props => props.theme.background.default};
